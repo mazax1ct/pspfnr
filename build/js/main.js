fc59bf5e2b656a49d357b1b3b35e886927439cd7
mazax1ct <vova@giveandget.ru>
2021-09-06 17:25:51 +0300

    progs + some pages

--- build/js/main.js
+++ build/js/main.js
@@ -16,8 +16,65 @@ $(document).ready(function() {
 		$p.removeClass('open');
 	});
 
-	//блок слайдер
-	if($()) {
+	//запуск плавающего левого меню
+  if ($("#float-nav").length) {
+    if ($("body").width() >= 1200) {
+      $(".js-sticky-block").trigger("sticky_kit:detach");
+      setTimeout(function() {
+        $(".js-sticky-block").stick_in_parent({
+          offset_top: 160
+        });
 
-	}
+        //навигация по якорям
+        $("#float-nav").ddscrollSpy({
+          scrolltopoffset: -160
+        });
+      }, 100);
+    }
+
+    //если блок для контента пустой, открепляем плавающее левое меню
+    if ($(".js-content-with-sticky").length) {
+      if ($('.js-content-with-sticky').html().trim() === '') {
+        $(".js-sticky-block").trigger("sticky_kit:detach");
+      }
+    }
+  }
+});
+
+//открепляем и перезапускаем прилипающий блок при резайзе
+$(window).resize(function() {
+  if ($("#float-nav").length) {
+    if ($("body").width() >= 1200) {
+      $(".js-sticky-block").trigger("sticky_kit:detach");
+      setTimeout(function() {
+        $(".js-sticky-block").stick_in_parent({
+          offset_top: 160
+        });
+
+        //навигация по якорям в новости
+        $("#float-nav").ddscrollSpy({
+          scrolltopoffset: -160
+        });
+      }, 100);
+    }
+  }
+});
+
+//открепляем и перезапускаем прилипающий блок при повороте устройства
+$(window).on("orientationchange", function(event) {
+  if ($("#float-nav").length) {
+    if ($("body").width() >= 1200) {
+      $(".js-sticky-block").trigger("sticky_kit:detach");
+      setTimeout(function() {
+        $(".js-sticky-block").stick_in_parent({
+          offset_top: 160
+        });
+
+        //навигация по якорям в новости
+        $("#float-nav").ddscrollSpy({
+          scrolltopoffset: -160
+        });
+      }, 100);
+    }
+  }
 });
