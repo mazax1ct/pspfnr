7ee918888ff9a907dc7851f0003fede6716e0c4f
mazax1ct <vova@giveandget.ru>
2022-06-20 15:37:54 +0300

    fancebox for iamges gallery

---
+++ build/js/fancybox-defaults.js
@@ -0,0 +1,6 @@
+$.fancybox.defaults.i18n.en.CLOSE = "Закрыть";
+$.fancybox.defaults.i18n.en.NEXT = "Вперёд";
+$.fancybox.defaults.i18n.en.PREV = "Назад";
+$.fancybox.defaults.infobar = false;
+$.fancybox.defaults.smallBtn = true;
+$.fancybox.defaults.autoFocus = false;
